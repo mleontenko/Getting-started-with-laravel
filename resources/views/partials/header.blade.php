992ceef1dcbfcc710f809b3aa3b726d32cd73a7b
Marin Leontenko <marin.leontenko@gmail.com>
2017-05-01 17:57:32 +0200

    Change title

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a href="{{ route('blog.index') }}" class="navbar-brand">Laravel Guide</a>
+            <a href="{{ route('blog.index') }}" class="navbar-brand">Blog app</a>
             <ul class="nav navbar-nav">
                 <li><a href="{{ route('blog.index') }}">Blog</a></li>
                 <li><a href="{{ route('other.about') }}">About</a></li>

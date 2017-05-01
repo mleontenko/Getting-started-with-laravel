d62c013698dba5151b56c33d910fa64c2f41f480
Marin Leontenko <marin.leontenko@gmail.com>
2017-05-01 17:41:55 +0200

    Update index.blade.php in admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     @if(Session::has('info'))
-      <div class="row">
-        <div class="col-md-12">
-          <p class="alert alert-info">{{ Session::get('info') }}</p>
+        <div class="row">
+            <div class="col-md-12">
+                <p class="alert alert-info">{{ Session::get('info') }}</p>
+            </div>
         </div>
-      </div>
     @endif
     <div class="row">
         <div class="col-md-12">
@@ -14,9 +14,11 @@
         </div>
     </div>
     <hr>
+    @foreach($posts as $post)
     <div class="row">
         <div class="col-md-12">
-            <p><strong>Learning Laravel</strong> <a href="{{ route('admin.edit', ['id' => 1]) }}">Edit</a></p>
+            <p><strong>{{ $post['title'] }}</strong> <a href="{{ route('admin.edit', ['id' => array_search($post, $posts)]) }}">Edit</a></p>
         </div>
     </div>
+    @endforeach
 @endsection
